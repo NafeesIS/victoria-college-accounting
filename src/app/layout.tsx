c5835200f48c42225e09addf2194cc4c0cf91257
NafeesIS <nafeesshawon555@gmail.com>
2025-09-18 13:01:24 +0600

    updated layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
+import "./globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
